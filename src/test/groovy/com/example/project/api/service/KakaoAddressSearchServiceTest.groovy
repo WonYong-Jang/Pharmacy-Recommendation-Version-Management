6d2af23941f4bdd39015e494134a65ca5be9f887
wonyong <zcx5674@gmail.com>
2022-10-16 23:22:33 +0900

    Ch05-06.Kakao 주소검색 api 테스트 코드 작성하기

---
+++ src/test/groovy/com/example/project/api/service/KakaoAddressSearchServiceTest.groovy
@@ -0,0 +1,34 @@
+package com.example.project.api.service
+
+import com.example.project.AbstractIntegrationContainerBaseTest
+import org.springframework.beans.factory.annotation.Autowired
+
+class KakaoAddressSearchServiceTest extends AbstractIntegrationContainerBaseTest {
+
+    @Autowired
+    private KakaoAddressSearchService kakaoAddressSearchService
+
+    def "address 파라미터 값이 null이면, requestAddressSearch 메소드는 null을 리턴한다."() {
+        given:
+        String address = null
+
+        when:
+        def result = kakaoAddressSearchService.requestAddressSearch(address)
+
+        then:
+        result == null
+    }
+
+    def "주소값이 valid하다면, requestAddressSearch 메소드는 정상적으로 document를 반환한다."() {
+        given:
+        def address = "서울 성북구 종암로 10길"
+
+        when:
+        def result = kakaoAddressSearchService.requestAddressSearch(address)
+
+        then:
+        result.documentList.size() > 0
+        result.metaDto.totalCount > 0
+        result.documentList.get(0).addressName != null
+    }
+}
--- src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -9,6 +9,10 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
     @Autowired
     private PharmacyRepository pharmacyRepository
 
+    def setup() {
+        pharmacyRepository.deleteAll()
+    }
+
     def "PharmacyRepository save"() {
         given:
         String address = "서울 특별시 성북구 종암동"
@@ -31,5 +35,27 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         result.getPharmacyName() == name
         result.getLatitude() == latitude
         result.getLongitude() == longitude
+    }
+
+    def "PharmacyRepository saveAll"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyRepository.saveAll(Arrays.asList(pharmacy))
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.size() == 1
     }
 }
