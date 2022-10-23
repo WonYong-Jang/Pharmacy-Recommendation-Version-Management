a456a4a4bf5478cfebcb422ec1254a099ff17c1b
wonyong <zcx5674@gmail.com>
2022-10-23 16:24:19 +0900

    Ch05-10.Spring Transactional 사용시 주의 사항

--- src/main/java/com/example/project/pharmacy/service/PharmacyRepositoryService.java
+++ src/main/java/com/example/project/pharmacy/service/PharmacyRepositoryService.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
@@ -14,6 +16,32 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class PharmacyRepositoryService {
     private final PharmacyRepository pharmacyRepository;
+
+    // self invocation test
+    @Transactional
+    public void bar(List<Pharmacy> pharmacyList) {
+        log.info("bar CurrentTransactionName: "+ TransactionSynchronizationManager.getCurrentTransactionName());
+        foo(pharmacyList);
+    }
+
+    // self invocation test
+    @Transactional
+    public void foo(List<Pharmacy> pharmacyList) {
+        log.info("foo CurrentTransactionName: "+ TransactionSynchronizationManager.getCurrentTransactionName());
+        pharmacyList.forEach(pharmacy -> {
+            pharmacyRepository.save(pharmacy);
+            throw new RuntimeException("error"); // 예외 발생
+        });
+    }
+
+
+    // read only test
+    @Transactional(readOnly = true)
+    public void startReadOnlyMethod(Long id) {
+        pharmacyRepository.findById(id).ifPresent(pharmacy ->
+                pharmacy.changePharmacyAddress("서울 특별시 광진구"));
+    }
+
 
     @Transactional
     public void updateAddress(Long id, String address) {
@@ -37,5 +65,10 @@ public class PharmacyRepositoryService {
         }
 
         entity.changePharmacyAddress(address);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Pharmacy> findAll() {
+        return pharmacyRepository.findAll();
     }
 }
--- src/test/groovy/com/example/project/pharmacy/service/PharmacyRepositoryServiceTest.groovy
+++ src/test/groovy/com/example/project/pharmacy/service/PharmacyRepositoryServiceTest.groovy
@@ -4,7 +4,6 @@ import com.example.project.AbstractIntegrationContainerBaseTest
 import com.example.project.pharmacy.entity.Pharmacy
 import com.example.project.pharmacy.repository.PharmacyRepository
 import org.springframework.beans.factory.annotation.Autowired
-import spock.lang.Specification
 
 class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest {
 
@@ -57,6 +56,55 @@ class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest
         def result = pharmacyRepository.findAll()
 
         then:
+        result.get(0).getPharmacyAddress() == inputAddress
+    }
+
+    def "self invocation"() {
+
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyRepositoryService.bar(Arrays.asList(pharmacy))
+
+        then:
+        def e = thrown(RuntimeException.class)
+        def result = pharmacyRepositoryService.findAll()
+        result.size() == 1 // 트랜잭션이 적용되지 않는다( 롤백 적용 X )
+    }
+
+    def "transactional readOnly test - 읽기 전용일 경우 dirty checking 반영 되지 않는다. "() {
+
+        given:
+        String inputAddress = "서울 특별시 성북구"
+        String modifiedAddress = "서울 특별시 광진구"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def input = Pharmacy.builder()
+                .pharmacyAddress(inputAddress)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        def pharmacy = pharmacyRepository.save(input)
+        pharmacyRepositoryService.startReadOnlyMethod(pharmacy.id)
+
+        then:
+        def result = pharmacyRepositoryService.findAll()
         result.get(0).getPharmacyAddress() == inputAddress
     }
 }
